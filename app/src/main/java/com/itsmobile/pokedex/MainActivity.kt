0574031b4597541569b68bcb9484dc5bb6340253
Davide <69810644+ItzDavi@users.noreply.github.com>
2023-05-01 21:37:40 +0200

    Project setup

--- app/src/main/java/com/itsmobile/pokedex/MainActivity.kt
+++ app/src/main/java/com/itsmobile/pokedex/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.itsmobile.pokedex
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.itsmobile.pokedex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
