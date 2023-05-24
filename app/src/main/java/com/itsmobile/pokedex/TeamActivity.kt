d3074deb1e40bc8736c47a05dcbf39bf0e1e2390
Davide <69810644+ItzDavi@users.noreply.github.com>
2023-05-24 17:36:33 +0200

    Added pokeball animations

--- app/src/main/java/com/itsmobile/pokedex/TeamActivity.kt
+++ app/src/main/java/com/itsmobile/pokedex/TeamActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -24,19 +25,41 @@ class TeamActivity : AppCompatActivity() {
         window.statusBarColor = getColor(R.color.primary)
         onBackPressedDispatcher.addCallback(this, onBackPressedCallBack)
 
+        val shakeLeftAnim = AnimationUtils.loadAnimation(this@TeamActivity, R.anim.shake_left)
+        val shakeRightAnim = AnimationUtils.loadAnimation(this@TeamActivity, R.anim.shake_right)
+        val shakeToCenterLeftAnim = AnimationUtils.loadAnimation(this@TeamActivity, R.anim.shake_to_center_left)
+        val shakeToCenterRightAnim = AnimationUtils.loadAnimation(this@TeamActivity, R.anim.shake_to_center_right)
+
+        val pokeballList = listOf(binding.pokeball1, binding.pokeball2, binding.pokeball3, binding.pokeball4, binding.pokeball5, binding.pokeball6)
+        val viewList = listOf(binding.pokemon1, binding.pokemon2, binding.pokemon3, binding.pokemon4, binding.pokemon5, binding.pokemon6)
+
         val r = findViewById<ImageButton>(R.id.randomButton)
         r.setOnClickListener {
-            resetVisibility()
+            var animCounter = 0
 
-            val fadeInAnim = AnimationUtils.loadAnimation(this@TeamActivity, R.anim.fade_in_anim)
-            val viewList = listOf(binding.pokemon1, binding.pokemon2, binding.pokemon3, binding.pokemon4, binding.pokemon5, binding.pokemon6)
+            lifecycleScope.launch {
+                while (animCounter != viewList.size) {
+                    for (i in 1..2) {
+                        pokeballList[animCounter].startAnimation(shakeRightAnim)
+                        delay(shakeRightAnim.duration)
+                        // Animate pokeball back to center
+                        pokeballList[animCounter].startAnimation(shakeToCenterRightAnim)
+                        delay(shakeToCenterRightAnim.duration)
 
-            for (view in viewList) {
-                lifecycleScope.launch {
-                    Glide.with(this@TeamActivity).load(getRandomPokemon()).centerInside().into(view)
-                    view.startAnimation(fadeInAnim)
-                    view.visibility = View.VISIBLE
-                    delay(1000)
+                        // Animate pokeball to the left
+                        pokeballList[animCounter].startAnimation(shakeLeftAnim)
+                        delay(shakeRightAnim.duration)
+                        // Animate pokeball back to center
+                        pokeballList[animCounter].startAnimation(shakeToCenterLeftAnim)
+                        delay(shakeToCenterRightAnim.duration)
+                    }
+
+                    Glide.with(this@TeamActivity).load(getRandomPokemon()).centerInside().into(viewList[animCounter])
+                    viewList[animCounter].visibility = View.VISIBLE
+
+                    delay(500)
+
+                    animCounter++
                 }
             }
         }
@@ -47,15 +70,6 @@ class TeamActivity : AppCompatActivity() {
         }
     }
 
-    private fun resetVisibility() {
-        val fadeOutAnim = AnimationUtils.loadAnimation(this@TeamActivity, R.anim.fade_out_anim)
-        val viewList = listOf<View>(binding.pokemon1, binding.pokemon2, binding.pokemon3, binding.pokemon4, binding.pokemon5, binding.pokemon6)
-
-        for (view in viewList) {
-            view.startAnimation(fadeOutAnim)
-            view.visibility = View.INVISIBLE
-        }
-    }
 
     private fun getRandomPokemon(): String {
         val rand = Random.nextInt(1, 1000)
